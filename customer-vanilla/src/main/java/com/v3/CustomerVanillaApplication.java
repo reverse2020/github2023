be7616fb82d67157957f611d7110c78ca4b6689c
Loveleen GitHub <loveleen.narang@gmail.com>
2023-07-08 12:21:55 -0400

    up

--- customer-vanilla/src/main/java/com/v3/CustomerVanillaApplication.java
+++ customer-vanilla/src/main/java/com/v3/CustomerVanillaApplication.java
@@ -8,7 +8,8 @@ public class CustomerVanillaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerVanillaApplication.class, args);
-	//test commit
+	//test commit 
+		
 	}
 
 }
