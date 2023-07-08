0dd1c7790ab694475dd6c3f300124708a452f507
Loveleen GitHub <loveleen.narang@gmail.com>
2023-07-08 12:14:48 -0400

    vanilla-commit

---
+++ customer-vanilla/src/main/java/com/v3/CustomerVanillaApplication.java
@@ -0,0 +1,13 @@
+package com.v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerVanillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerVanillaApplication.class, args);
+	}
+
+}
---
+++ customer-vanilla/src/test/java/com/v3/CustomerVanillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerVanillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
